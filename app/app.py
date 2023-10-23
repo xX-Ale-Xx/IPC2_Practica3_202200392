7c4afbc777fe12efab06f422b0e5354c80f1f0f2
xX-Ale-Xx <floresjavier092@gmail.com>
2023-10-22 20:28:50 -0600

    Agregando env

---
+++ app/app.py
@@ -0,0 +1,36 @@
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+movies = []
+@app.route("/")
+def index():
+    return "Bienvenido"
+
+@app.route("/api/new-movie", methods=["POST"])
+def add_movie():
+    movie_data = request.get_json()
+    movies.append(movie_data)
+    return json.dumps({"message": "La película fue agregada con éxito"})
+
+@app.route("/api/all-movies-by-genre/<genre>", methods=["GET"])
+def get_movies_by_genre(genre):
+    filtered_movies = [movie for movie in movies if movie["genre"] == genre]
+
+    # Devolver una respuesta
+    return json.dumps(filtered_movies)
+
+@app.route("/api/update-movie", methods=["PUT"])
+def update_movie():
+    movie_data = request.get_json()
+    for movie in movies:
+        if movie["movieId"] == movie_data["movieId"]:
+            movie["name"] = movie_data["name"]
+            movie["genre"] = movie_data["genre"]
+            break
+
+    
+    return json.dumps({"message": "La película fue actualizada con éxito"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
